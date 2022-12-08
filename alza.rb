64deee2959f7bd3f204204fde68e6712fe145ca8
Rattlehead90 <strogonov@seznam.cz>
2022-12-08 15:19:40 +0200

    Make alza more readable

--- alza.rb
+++ alza.rb
@@ -4,9 +4,13 @@ require 'csv'
 require_relative 'table'
 require_relative 'array_builder'
 
-CSV.open('trhaky.csv', 'w', headers: true) do |csv|
-  csv << ['Name', 'Price']
-  array_builder(2).each { |array| csv << array }
+def scraper(num_of_pages)
+  CSV.open('trhaky.csv', 'w', headers: true) do |csv|
+    csv << ['Name', 'Price']
+    array_builder(num_of_pages).each { |array| csv << array }
+  end
+
+  table_out_of('trhaky.csv')
 end
 
-table_out_of('trhaky.csv')
+scraper(2) # <-- Change the number of pages you'd like to scrape here
